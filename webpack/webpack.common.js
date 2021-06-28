0f5206a8825a09f4a49ec570a275ec870928efe0
flaaaps <julian@wening.me>
2021-06-28 14:58:20 +0200

    Create webpack configuration files

---
+++ webpack/webpack.common.js
@@ -0,0 +1,39 @@
+const path = require("path")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
+const srcDir = "../src/"
+
+module.exports = {
+    entry: {
+        content: path.join(__dirname, srcDir + "content.ts"),
+        main: path.join(__dirname, srcDir + "scripts/main.ts"),
+        background: path.join(__dirname, srcDir + "background.ts"),
+    },
+    devtool: "inline-source-map",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+    output: {
+        path: path.resolve(__dirname, "../dist"),
+        filename: "[name].js",
+        library: "pmd",
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: "manifest.json", to: "manifest.json" },
+                { from: "assets", context: "src" },
+                { from: "views", context: "src" },
+                { from: "styles", context: "src" },
+            ],
+        }),
+    ],
+}
